c6538b476fd5dd13e97d1fb8e9a687ff01dcc180
kmajka <kfmajka@gmail.com>
2019-05-02 15:02:44 +0200

    new page

---
+++ src/main/java/com/thymeleaf/example/mydemo/Controller/InfoController.java
@@ -0,0 +1,29 @@
+package com.thymeleaf.example.mydemo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.jws.WebMethod;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class InfoController {
+
+    @RequestMapping("/infom")
+    public String infoMethod() {
+        return "infopage.html"; //view
+    }
+
+    @RequestMapping("/infot")
+    public String infoMethodThymeLeaf(Model model) {
+        List<String> list = new ArrayList<String>(Arrays.asList(new String[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" , "..."}));
+        model.addAttribute("listabc", list);
+        model.addAttribute("name", "Letter:");
+        return "thymeleafpage.html"; //view
+    }
+
+}
