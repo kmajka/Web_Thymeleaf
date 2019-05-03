fad36d6d9346192ef914d984128c3d7143539304
kmajka <kfmajka@gmail.com>
2019-05-03 12:55:29 +0200

    commit;

--- src/main/java/com/thymeleaf/example/mydemo/controllers/InfoController.java
+++ src/main/java/com/thymeleaf/example/mydemo/controllers/InfoController.java
@@ -1,11 +1,9 @@
-package com.thymeleaf.example.mydemo.Controller;
+package com.thymeleaf.example.mydemo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.jws.WebMethod;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
